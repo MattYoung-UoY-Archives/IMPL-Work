1a382f95601be99687ea426bc2114a9a987828fd
mattyoung-uoy <mjy503@york.ac.uk>
2020-01-22 09:47:31 +0000

    Continued work on the LexerCombinator.hs file for excercise 11.1

--- chp11combinators/LexerCombinator.hs
+++ chp11combinators/LexerCombinator.hs
@@ -5,3 +5,18 @@ type RegExp a = [a] -> (Maybe [a], [a])
 re_x :: RegExp Char
 re_x ('x':t) = (Just "x", t)
 re_x s = (Nothing, s)
+
+re_ws :: RegExp Char
+re_ws (' ':t) = (Just " ", t)
+re_ws s = (Nothing, s)
+
+chop :: (Eq a, Show a) => RegExp a -> RegExp a -> [a] -> [[a]]
+-- chop regexpWhitespace regexpTokens source -> listOfTokens
+chop w r = rrw
+	where
+		rrw s   | s2 == [] = [] -- nothing left to process
+			| isJust m = fromJust m : rrw t -- regexp found
+			| otherwise = error ("\nlexing error : " ++ show (take 8 t ))
+				where
+					(_, s2 ) = w s -- swallow white space
+					(m, t ) = r s2 -- grab next lexeme
